cbda1b0e759a145732f5070a0cdb2c8744d4f82d
lttvi <lttvi1822@gmail.com>
2021-06-30 19:20:29 +0700

    initial commit

---
+++ ECommerce/Models/Admin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Models
+{
+    public class Admin
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public Admin(int id, string username, string password, string name, string email)
+        {
+            this.id = id;
+            this.username = username;
+            this.password = password;
+            this.name = name;
+            this.email = email;
+        }
+
+    }
+}
